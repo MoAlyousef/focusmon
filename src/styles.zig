ebf5b6099dd436e81c4b2d7c5b5630797977cc6e
MoAlyousef <may642_2000@hotmail.com>
2023-07-01 04:59:24 +0300

    use setColorAny

--- src/styles.zig
+++ src/styles.zig
@@ -8,7 +8,7 @@ pub fn styleApp() void {
     app.setBackground(enums.Color.fromRgb(0x1d, 0x1f, 0x23));
     app.setBackground2(enums.Color.fromRgb(0x28, 0x2c, 0x34));
     app.setForeground(enums.Color.fromRgb(255, 255, 255));
-    app.setColor(15, enums.Color.fromHex(0xFFA500));
+    app.setColorAny(15, enums.Color.fromHex(0xFFA500));
     app.setFontSize(14);
     app.setMenuLinespacing(8);
 }
